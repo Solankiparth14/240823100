71c2bc61ab405d02b4804e70710407d5c1801e80
Solankiparth14 <parthsolanki1808@gmail.com>
2025-07-17 08:49:30 +0530

    aa

--- index.js
+++ index.js
@@ -1,48 +1,72 @@
 
 //1
-a=7
-if(a%2==0){
-    console.log('even')
-}
-else{
-    console.log('odd')
-}
+// a=7
+// if(a%2==0){
+//     console.log('even')
+// }
+// else{
+//     console.log('odd')
+// }
 
 //2
 
- a=5
- b=9
- if(a<b)
- {
-     console.log('larger number')
- }
- else
-{
-    console.log('small number')
-}
+//  a=5
+//  b=9
+//  if(a<b)
+//  {
+//      console.log('larger number')
+//  }
+//  else
+// {
+//     console.log('small number')
+// }
 
 
 // 3
 
-leap=2020
+// leap=2020
 
-if(leap%4==0)
-{
-    console.log('leap year')
-}
-else{
-    console.log('year')
-}
+// if(leap%4==0)
+// {
+//     console.log('leap year')
+// }
+// else{
+//     console.log('year')
+// }
 
-//4
+// //4
 
 
-num=5
-sum=0
+// num=5
+// sum=0
 
-for(i=1;i<=num;i++){
+// for(i=1;i<=num;i++){
     
-    sum=sum+i
+//     sum=sum+i
     
-}
-   console.log(sum)
+// }
+//    console.log(sum)
+
+// //  5
+
+// const fac=5
+// var sum=1
+
+//  for(i=fac ; i>1;i--)
+//  {
+//      sum=i*sum
+//  }
+//  console.log(sum)
+
+//  6
+ 
+//  a=5
+
+
+// for(i=1;i<=10;i++)
+// {
+//     console.log(a+"*"+i+"="+i*i)
+// }
+
+
+// 7
